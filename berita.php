74c711122350ed3fae6cea80140f049de44ab5dd
VlinZZ <VlinZZ>
2024-10-07 12:52:40 +0700

    ganti posisi file

---
+++ berita.php
@@ -0,0 +1,44 @@
+<div class="container mt-5">
+    <h2 class="mb-3">Berita</h2>
+    <a href="tambah_berita.php" class="btn btn-primary mb-3">Tambah Berita</a>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Berita 1: SMKN 2 Padang Gelar Acara Wisuda</h5>
+                    <p class="card-text">SMKN 2 Padang telah menggelar acara wisuda bagi siswa-siswi yang telah lulus.</p>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Berita 2: SMKN 2 Padang Raih Juara 1 Lomba Robotika</h5>
+                    <p class="card-text">SMKN 2 Padang telah meraih juara 1 dalam lomba robotika yang diadakan di Jakarta.</p>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Berita 3: SMKN 2 Padang Gelar Acara Seminar</h5>
+                    <p class="card-text">SMKN 2 Padang telah menggelar acara seminar yang dihadiri oleh siswa-siswi dan guru.</p>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Berita 4: SMKN 2 Padang Raih Penghargaan dari Pemerintah</h5>
+                    <p class="card-text">SMKN 2 Padang telah meraih penghargaan dari pemerintah atas prestasinya dalam bidang pendidikan.</p>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
