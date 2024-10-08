54365e14dba8b5550167e21a6b9334f5656c1a02
VlinZZ <VlinZZ>
2024-10-08 15:36:02 +0700

    Berita Telah Di Buat

--- berita.php
+++ berita.php
@@ -1,44 +1,74 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<style>
+    .card-img-overlay {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: rgba(0, 0, 0, 0.5); /* Background transparan */
+        color: white; /* Warna teks putih */
+        padding: 10px;
+    }
+
+    .card img {
+        width: 100%;
+        height: auto;
+    }
+
+    .card {
+        position: relative;
+    }
+</style>
+
 <div class="container mt-5">
-    <h2 class="mb-3">Berita</h2>
-    <a href="tambah_berita.php" class="btn btn-primary mb-3">Tambah Berita</a>
+    <h1 class="mb-3"><center><b>Berita</b></center></h1>
+    <a href="Tambah_Berita.html" class="btn fw-medium text-light mb-3 bg-success">Tambah Berita</a>
     <div class="row">
         <div class="col-md-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Berita 1: SMKN 2 Padang Gelar Acara Wisuda</h5>
-                    <p class="card-text">SMKN 2 Padang telah menggelar acara wisuda bagi siswa-siswi yang telah lulus.</p>
-                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+            <a href="isi berita/joko.html">
+                <div class="card">
+                    <img src="fto/joko.jpeg" class="card-img-top" alt="Foto Berita 1">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Virall Joko widodo...</h5>
+                        <p class="card-text">Jokowi Tidak Salami Mantan Wapres Try Sutrisno, Istana Beri Klarifikasi</p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
         <div class="col-md-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Berita 2: SMKN 2 Padang Raih Juara 1 Lomba Robotika</h5>
-                    <p class="card-text">SMKN 2 Padang telah meraih juara 1 dalam lomba robotika yang diadakan di Jakarta.</p>
-                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+            <a href="isi berita/pbwo.html">
+                <div class="card">
+                    <img src="fto/pbwo.jpeg" class="card-img-top" alt="Foto Berita 2">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Untung Rugi Jika Gugatan Pencalonan Gibran Dikabulkan PTUN Jakarta</h5>
+                        <p class="card-text">Majelis hakim Pengadilan Tata Usaha Negara atau PTUN Jakarta akan membacakan putusan...</p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     </div>
     <div class="row">
         <div class="col-md-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Berita 3: SMKN 2 Padang Gelar Acara Seminar</h5>
-                    <p class="card-text">SMKN 2 Padang telah menggelar acara seminar yang dihadiri oleh siswa-siswi dan guru.</p>
-                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+            <a href="isi berita/bla.html">
+                <div class="card">
+                    <img src="fto/bla.jpeg" class="card-img-top" alt="Foto Berita 3">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Barcelona Dikabarkan akan Rekrut...</h5>
+                        <p class="card-text">Neymar Jr Sebelum Datangkan Bintang Manchester City,Erling Haaland</p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
         <div class="col-md-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Berita 4: SMKN 2 Padang Raih Penghargaan dari Pemerintah</h5>
-                    <p class="card-text">SMKN 2 Padang telah meraih penghargaan dari pemerintah atas prestasinya dalam bidang pendidikan.</p>
-                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+            <a href="isi berita/blo.html">
+                <div class="card">
+                    <img src="fto/blo.jpeg" class="card-img-top" alt="Foto Berita 4">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Berita 4: SMKN 2 Padang Raih Penghargaan dari Pemerintah</h5>
+                        <p class="card-text">SMKN 2 Padang telah meraih penghargaan dari pemerintah atas prestasinya dalam bidang pendidikan.</p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     </div>
 </div>
