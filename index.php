9d87d2408ee1f6fa989a84654a03ec9c740a5aca
rell-za <rellultra@gmail.com>
2024-10-08 14:26:47 +0700

    selesai regis

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>navbar</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+<header class="bg-dark text-white p-3 text-center">
+        <img src="logo-sekolah.png" alt="Logo Sekolah" height="80" class="mr-3">
+    </header>
+
+    <nav class="bg-light p-3 text-center">
+        <a href="#" class="mr-3">Home</a>
+        <a href="#" class="mr-3">Tim</a>
+        <a href="#" class="mr-3">Berita</a>
+        <a href="#" class="mr-3">Dashboard</a>
+        <a href="#">Log out</a>
+    </nav>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="fto/kelompok.jpg" alt="Anggota" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <h2 class="mb-3">Hai Selamat Datang Di Website</h2>
+                <p class="text-muted">"Hasil Kerja Kami Bertiga"</p>
+                <p>Disini Anda Dapat Menemukan Informasi Tentang Anggota Kami dan
+                    Beberapa Berita YAng Telah Kami Buat Sedemikian Rupa, Dan Kedepa
+                     Nya Anda Juga Dapat Menambah Berita Di Dalam Website Ini.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
