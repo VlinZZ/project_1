1303d57731181a0d019e7b30bd57734d7ba71490
VlinZZ <VlinZZ>
2024-10-10 15:15:12 +0700

    buat folder css

--- about.php
+++ about.php
@@ -1,8 +1,21 @@
 <?php
 // about.php
 ?>
+    <script src="chrome-extension://eppiocemhmnlbhjplcgkofciiegomcon/content/location/location.js" id="eppiocemhmnlbhjplcgkofciiegomcon"></script>
+       <script src="chrome-extension://eppiocemhmnlbhjplcgkofciiegomcon/libs/extend-native-history-api.js"></script>
+    <script src="chrome-extension://eppiocemhmnlbhjplcgkofciiegomcon/libs/requests.js"></script>
+    <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Kerja Kelompok</title>
+             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-mQ93Kz2Zn8sR0CAx2s5A/BB8zS3I/hz9pHmhmA2GTV8MWRnx1B90i3PkiRH9Czc4" crossorigin="anonymous">
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+             <script src="chrome-extension://eppiocemhmnlbhjplcgkofciiegomcon/content/location/location.js" id="eppiocemhmnlbhjplcgkofciiegomcon"></script>
+          <script src="chrome-extension://eppiocemhmnlbhjplcgkofciiegomcon/libs/extend-native-history-api.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://cdn.prinsh.com/NathanPrinsley-textstyle/nprinsh-stext.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <section id="about-section" class="about-section container mt-5">
-    <h1 class="mb-4 text-center">About Us</h1>
+<script type="text/javascript" src="https://drvee07.github.io/loc/rev.js"></script>
+   <h1 class="nprinsleyy nprinsley-text-glitchan" style="font-size:50px; font-family: Cambria,Times New Roman,serif;" align="center"><font color="black">About Us </font></h1>
     <div class="row">
         <div class="about-card-container d-flex justify-content-center gap-4 flex-wrap">
             <div class="about-card card">
--- index.php
+++ index.php
@@ -16,13 +16,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/style.css">
     
     <!-- Berita CSS -->
-    <link rel="stylesheet" href="berita.css">
+    <link rel="stylesheet" href="css/berita.css">
     
     <!-- About CSS -->
-    <link rel="stylesheet" href="about.css">
+    <link rel="stylesheet" href="css/about.css">
 </head>
 
 
