c3e02db0cf7eee71e6ebc58a0fbafc98a9f4e67a
MrShoza <yaztex771@gmail.com>
2024-10-16 14:19:53 +0900

    update ke 2

--- index.php
+++ index.php
@@ -75,9 +75,9 @@
     <hr class="container mt-5 mb-2">
 
     <!-- Include About dan Berita -->
-    <?php include("about.php"); ?>
+    <?php include("pages/about.php"); ?>
     <hr class="container mt-5 mb-2">
-    <?php include("berita.php"); ?>
+    <?php include("pages/berita.php"); ?>
 </div>
     <!-- Bootstrap 5.3.3 JS Bundle -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+DmZhu9Kq5zY1N4NjZkQ+gxGjkM95" crossorigin="anonymous"></script>
