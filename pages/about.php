551b0aeb85369e2328d64d4beee2ddf3aaccfca3
MrShoza <yaztex771@gmail.com>
2024-10-16 14:12:11 +0900

    Sudah Update SayoNara

--- pages/about.php
+++ pages/about.php
@@ -20,29 +20,29 @@
     <div class="row">
         <div class="about-card-container d-flex justify-content-center gap-4 flex-wrap">
         <div class="about-card card">
-                <img src="assets/revalino.jpg" class="card-img-top" alt="Revalino Jihadil Lopa">
+                <img src="../assets/revalino.jpg" class="card-img-top" alt="Revalino Jihadil Lopa">
                 <div class="card-body">
                     <h3 class="card-title">Revalino Jihadil Lopa</h3>
                     <p class="card-text">Informasi tentang Saya</p>
-                    <a href="about/valin.php" class="btn btn-about">Lihat</a>
+                    <a href="../about/valin.php" class="btn btn-about">Lihat</a>
                 </div>
             </div>
 
             <div class="about-card card">
-                <img src="assets/farel.jpg" class="card-img-top" alt="Farel Yuhandriza Putra">
+                <img src="../assets/farel.jpg" class="card-img-top" alt="Farel Yuhandriza Putra">
                 <div class="card-body">
                     <h3 class="card-title">Farel Yuhandriza Putra</h3>
                     <p class="card-text">Informasi tentang Saya</p>
-                    <a href="about/rell.php" class="btn btn-about">Lihat</a>
+                    <a href="../about/rell.php" class="btn btn-about">Lihat</a>
                 </div>
             </div>
 
             <div class="about-card card">
-                <img src="assets/taufiq.jpg" class="card-img-top" alt="Taufiq Hidayat">
+                <img src="../assets/taufiq.jpg" class="card-img-top" alt="Taufiq Hidayat">
                 <div class="card-body">
                     <h3 class="card-title">Taufiq Hidayat</h3>
                     <p class="card-text">Informasi tentang Saya</p>
-                    <a href="about/taufiq.php" class="btn btn-about">Lihat</a>
+                    <a href="../about/taufiq.php" class="btn btn-about">Lihat</a>
                 </div>
             </div>
         </div>
